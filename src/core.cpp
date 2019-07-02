2980a2e6da8b511053bf813a5215adb8be4c9475
educampos28 <educampos28@yahoo.com.br>
2019-07-02 10:30:11 -0400

    Update the setup procedures

--- src/core.cpp
+++ src/core.cpp
@@ -10,14 +10,17 @@
 
 
 #include "stdlib.h"
+#include "stdint.h"
 #include <stdio.h>
 #include <inttypes.h>
+
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include "SX1276.h"
 #include "Usart.h"
 #include "MS5607.h"
+#include <string.h>
 
 SX1276 LoRa;
 /******************************************************************
@@ -47,25 +50,36 @@ void setup(){
 	DDRB  |= (1<<PB3);
 	PORTB |= (1<<PB3);
 
-	if(!LoRa.Initialize(&USART,0))
+	if(!LoRa.Initialize(&USART,2,0x0F))
 		printf(PSTR("Lora Fail\n"));
 	else
 		printf_P(PSTR("Lora ok\n"));
+
+
+	DDRB   = ~(1<<PB4);
+	uint8_t status[50];
+	strcpy_P((char*)status,PSTR("Waiting the power up procedures to finish\n"));
+	while((PORTB & (1<<PB4))==0){
+		LoRa.Send(1,0x08,status,sizeof(status));
+		printf((const char*)status);
+		_delay_ms(1000);
+	}
+
+	strcpy_P((char*)status,PSTR("Ready to fly \n"));
+	printf((const char*)status);
 
 
 //	stdout = fdevopen(Std_putchar, NULL);
 	printf_P(PSTR("Setup Finished\n"));
 
 
-
 }
 
 void loop(){
 
-	PORTB &= ~(1<<PB5);
-	_delay_ms(1000);
-	PORTB |= (1<<PB5);
-	_delay_ms(1000);
+	uint8_t dados[10]={'o','i',0};
+	uint8_t tamanho=3;
+	LoRa.Send(1,0x08,dados,tamanho);
 
 }
 
